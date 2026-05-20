7e474673b1dbec50c23d1cf3ea83e28378303164
AkbarFelda <akbarfelda65@gmail.com>
2026-05-20 19:06:59 +0700

    [AF]: DELETE /authentications added middleware

--- src/api/authentications/routes.js
+++ src/api/authentications/routes.js
@@ -6,6 +6,6 @@ const handler = new AuthenticationsHandler();
 
 router.post('/', (req, res, next) => handler.postAuthenticationHandler(req, res, next));
 router.put('/', (req, res, next) => handler.putAuthenticationHandler(req, res, next));
-router.delete('/', (req, res, next) => handler.deleteAuthenticationHandler(req, res, next));
+router.delete('/', verifyToken, (req, res, next) => handler.deleteAuthenticationHandler(req, res, next));
 
 module.exports = router;
