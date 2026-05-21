2fe1593e6e81c06a5cde17cde9496c8a627dea22
AkbarFelda <akbarfelda65@gmail.com>
2026-05-21 11:38:17 +0700

    update import

--- src/api/authentications/routes.js
+++ src/api/authentications/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const AuthenticationsHandler = require('./handler');
-const { verifyToken } = require('../../middlewares/auth');
+const { verifyRefreshToken } = require('../../tokenize/TokenManager');
 
 const router = express.Router();
 const handler = new AuthenticationsHandler();
 
 router.post('/', (req, res, next) => handler.postAuthenticationHandler(req, res, next));
 router.put('/', (req, res, next) => handler.putAuthenticationHandler(req, res, next));
-router.delete('/', verifyToken, (req, res, next) => handler.deleteAuthenticationHandler(req, res, next));
+router.delete('/', verifyRefreshToken, (req, res, next) => handler.deleteAuthenticationHandler(req, res, next));
 
 module.exports = router;
